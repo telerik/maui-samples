176fb30f245b96312d7bc70503f2238432792c7a
Deyan Terziyski <93717765+ProgressiveDeyo@users.noreply.github.com>
2023-03-23 13:29:05 +0200

    Update CryptoTracker app to latest version.

--- Samples/CryptoTracker/App.xaml.cs
+++ Samples/CryptoTracker/App.xaml.cs
@@ -1,42 +1,54 @@
 ﻿using CryptoTracker.Data;
-using CryptoTracker.ViewModels;
 using CryptoTracker.Pages;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using Application = Microsoft.Maui.Controls.Application;
 
-namespace CryptoTracker
+namespace CryptoTracker;
+
+public partial class App : Application
 {
-	public partial class App : Application
+	private Page rootPage;
+	private Color barBackgroundColor = Color.FromArgb("#121212");
+	private Color barTextColor = Color.FromArgb("#FFFFFF");
+
+	public App()
 	{
-		public App()
-		{
-            this.UserAppTheme = Microsoft.Maui.ApplicationModel.AppTheme.Light;
-            Application.AccentColor = Color.FromArgb("#045DEA");
-			InitializeComponent();
-			DependencyService.Register<ICoinDataService, CoinDataService>();
-			// We use preprocessor directives in order to apply platform specific implementation.
+		this.UserAppTheme = Microsoft.Maui.ApplicationModel.AppTheme.Light;
+		Application.AccentColor = Color.FromArgb("#045DEA");
+		InitializeComponent();
+		DependencyService.Register<ICoinDataService, CoinDataService>();
+		// We use preprocessor directives in order to apply platform specific implementation.
+
+#if ANDROID || IOS
+		this.rootPage = new CoinSelectionPage();
+#else
+		this.rootPage = new DesktopPage();
+#endif
 
 #if MACCATALYST
-			MainPage = new DesktopPage();
-#elif WINDOWS
-			MainPage = new NavigationPage(new DesktopPage())
-			{
-				BarBackgroundColor = Colors.White,
-				BarTextColor = Color.FromArgb("#121212"),
-			};
+		MainPage = this.rootPage;
 #else
-			MainPage = new NavigationPage(new CoinSelectionPage())
-			{
-#if IOS
-			BarBackgroundColor = Colors.White,
-			BarTextColor = Color.FromArgb("#121212"),
-#else
-			BarBackgroundColor = Color.FromArgb("#121212"),
-			BarTextColor = Colors.White,
+		MainPage = new NavigationPage(this.rootPage)
+		{
+			BarBackgroundColor = barBackgroundColor,
+			BarTextColor = barTextColor
+		};
 #endif
-			};
-#endif
-		}
 	}
+
+#if NET7_0_OR_GREATER && (WINDOWS || MACCATALYST)
+		protected override Window CreateWindow(IActivationState activationState)
+		{
+			var window = base.CreateWindow(activationState);
+			if (window != null)
+			{
+				window.MinimumWidth = 1024;
+				window.MinimumHeight = 768;
+			}
+
+			return window;
+		}
+#endif
 }
